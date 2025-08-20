1d3cbb3815f06cc04f3e3af540d4f96c05528fb6
Francisco-Alamino-Neto <196649661+Francisco-Alamino-Neto@users.noreply.github.com>
2025-08-20 10:43:35 -0300

    fix: Main of package herancaVeiculos

--- src/herancaVeiculos/Main.java
+++ src/herancaVeiculos/Main.java
@@ -5,8 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Carro carro = new Carro("Corolla","Cinza");
+		Veiculos veiculo = new Veiculos("Civic", "Prata");
 		
 		carro.iniciar();
+		veiculo.iniciar();
 	}
 
 }
