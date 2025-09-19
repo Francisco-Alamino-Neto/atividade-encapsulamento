afda8f5f2ae6c37165a4605b0c0d5f377d7e0930
Francisco Alamino Neto <franciscoalaminoneto26@gmail.com>
2025-09-19 19:14:32 -0300

    Delete src/atividadeOrientacaoObjeto/Uchiha.java

--- src/atividadeOrientacaoObjeto/Uchiha.java
+++
@@ -1,9 +0,0 @@
-package atividadeOrientacaoObjeto;
-
-public class Uchiha extends Clans {
-
-	public Uchiha(String nomeClan, String personagens, String kekkeiGenkai) {
-		super(nomeClan, personagens, kekkeiGenkai);
-	}
-
-}
